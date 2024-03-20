93b17bcaf9445853609a47da953977e4b2e5dfbf
Owais Ahmed Khan <owais1566@gmail.com>
2024-03-20 10:29:18 +0500

    added Php basics

---
+++ PHP/basics/basics.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CLASS</title>
+</head>
+<body>
+<?php    
+    // BASICS
+    // echo "Welcome in PHP";
+    // VARIABLE DECLARATION $variableName
+    // $name = "Owais Ahmed Khan";
+    // $Name = "Owais";
+    // $Name = "Razi";
+    // $Name = "Fawad";
+    // $numb = -23;
+
+    // print($name);
+    // print($Name);
+    // print($numb);
+
+    // echo "<h1>My name is $name</h1>";
+
+    // Concatenation dot(.)
+    // echo "<h1>Sum of two numbers are: ". ($numb + 27) ."</h1>"
+
+    // Number Methods: abs, floor, ceil, sqrt, round
+    // echo abs($numb);
+    // echo floor(7.9);
+    // echo ceil(6.1);
+    // echo round(6.5);
+    // echo sqrt(625);
+
+    // string methods: ucwords(), strrev(), str_word_count()
+    // $str = "html css js bootstrap jQuery mysql php uiux seo vcs xml";
+
+    // echo $str;
+    // echo ucwords($str);
+    // echo strrev($str);
+    // echo str_word_count($str);
+
+    // Datatypes in PHP
+    // Number
+    $num = 30; //int
+    $num1 = 27.5; //float
+    $num2 = 25.8712647614; //double 
+
+    // String
+    $str = "html seo vcs xml"; //string
+
+    // Boolean: True ya False
+
+    $isValid = true;
+    $userResponse = false;
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
