7d99e704ca14b1ec2df073306f31e9ba0a482044
Owais Ahmed Khan <owais1566@gmail.com>
2024-03-22 16:51:17 +0500

    added syntax in php

--- PHP/basics/basics.php
+++ PHP/basics/basics.php
@@ -42,17 +42,111 @@
 
     // Datatypes in PHP
     // Number
-    $num = 30; //int
-    $num1 = 27.5; //float
-    $num2 = 25.8712647614; //double 
+    // $num = 30; //int
+    // $num1 = 27.5; //float
+    // $num2 = 25.8712647614; //double 
 
-    // String
-    $str = "html seo vcs xml"; //string
+    // // String
+    // $str = "html seo vcs xml"; //string
 
-    // Boolean: True ya False
+    // // Boolean: True ya False
 
-    $isValid = true;
-    $userResponse = false;
+    // $isValid = true;
+    // $userResponse = false;
+
+
+    // Array
+
+    // // $students = ["Ali", "Taha", "Karim"];
+
+    // $students =  array("Ali", "Taha", "Karim", "Abdullah", "Shayan");
+    //                 //   0      1      2        3
+
+    // echo $students[5];
+
+    // Functions
+    // fucntion declartion
+    // function add(){
+    //     echo 66 + 23;
+    // }
+
+    // // func calling
+    // add();
+
+    // function with parameters
+    // function add($a=12, $b=13){
+    //     echo $a + $b;
+    // }
+
+    // // calling func
+    // add();
+
+    // // function with parameters & arguments
+    // function add($a=0, $b=0){
+    //     echo $a + $b;
+    // }
+
+    // // calling func with arguments
+    // add(34, 55);
+
+
+    // function with parameters , arguments & return keyword
+    // function add($a=0, $b=0){
+    //     return $a + $b;
+    // }
+
+    // // calling func with arguments
+    // $res = add(44, 55);
+
+    // echo $res;
+
+    // conditional statements
+    $age = 21;
+
+    // if($age > 18){
+    //     echo "You are eligible for license";
+    // }
+    // else{
+    //     echo "You are not eligible for license";
+    // }
+
+    if($age > 18){
+        // echo "You are eligible for voting";
+        echo "<script>console.log('You are eligible for voting')</script>";
+        
+    }
+    elseif($age == 18){
+        // echo "<script>alert('Phle nic bnwao phr vote daldena')</script>";
+        echo "<h1>Phle nic bnwao phr vote daldena</h1>";
+    }
+    else{
+        echo "You are not eligible for voting";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
