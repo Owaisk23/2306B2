e475415f4d82060670de191d24c326550e3dfcbc
Owais Ahmed Khan <owais1566@gmail.com>
2024-06-29 17:51:21 +0500

    added insert product in Laravel

---
+++ Laravel/crud-laravel/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
