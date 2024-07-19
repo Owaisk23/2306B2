b3e6aec33d3e2285f11c54b9f94675f97eb83e7d
Owais Ahmed Khan <owais1566@gmail.com>
2024-07-19 10:49:33 +0500

    added Typescript class code

---
+++ TS/Basics/app.ts
@@ -0,0 +1,101 @@
+console.log("Hello world in TypeScript")
+
+// let userName: string = "Owais Ahmed Khan";
+// console.log(userName)
+// console.log(typeof userName)
+// console.log(userName.toLowerCase())
+// console.log(userName.toUpperCase())
+
+
+
+// let userAge: number = 23;
+// console.log("User age is " + userAge)
+// console.log(typeof userAge)
+
+
+// let userEmail: string = "owais@gmail.com"
+// let userDetails = `User Name is ${userName}, User Email is ${userEmail} & User Age is ${userAge}`;
+
+// console.log(userDetails);
+
+
+// let userPost = null;
+// console.log(userPost)
+// console.log(typeof userPost)
+
+// if(userPost){
+//     console.log("Null true huta hai")
+// }else{
+//     console.log("Null false huta hai")
+// }
+
+// let userPassword;
+
+// console.log(userPassword)
+// console.log(typeof userPassword)
+
+// if(userPassword){
+//     console.log("Undefined true huta hai")
+// }else{
+//     console.log("Undefined false huta hai")
+// }
+
+// creating object with any datatype
+
+// let products: any = {
+//     productName: "Iphone 14 Pro Max",
+//     productPrice: 350000,
+//     productColor: "Black"
+// }
+
+// console.log(products)
+// console.log(products.productName)
+// console.log(products.productPrice)
+// console.log(products.productColor)
+
+
+// Array in typescript
+
+let stdMarks: number[] = [90, 67, 95, 61, 98, 100];
+console.log(stdMarks)
+console.log(stdMarks[1])
+console.log(stdMarks[5])
+console.log(stdMarks[7])
+
+
+let stdName: string[] = ["Zohaib", "Saif", "Tayyaba", "Sameer"];
+console.log(stdName)
+console.log(stdName[0])
+console.log(stdName[1])
+console.log(stdName[5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
